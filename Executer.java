7986d4657aaa285a47da8e9955758d29f261914c
Prachi Rathore <rathoreprachi266@gmail.com>
2025-07-21 20:25:22 +0530

    Initial fix

--- Executer.java
+++ Executer.java
@@ -1,23 +1,41 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+
 public class Executer{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         reminderManager rm = new reminderManager();
 
-        DateTimeFormatter pattern = DateTimeFormatter.ofPattern(pattern:"yyyy-mm-dd");
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
         while(true){
-            System.out.println("**** Event Menu ****");
-            System.out.println("1. Add event");
-            System.out.println("2. Display events");
-            System.out.println("3. Show occured events");
-            System.out.println("4. Remove event");
-            System.out.println("5. Exit");
+            System.out.println("+----------------------------+");
+            System.out.println("|         Event Menu         |");
+            System.out.println("+----------------------------+");
+            System.out.println("|   1. Add event             |");
+            System.out.println("+----------------------------+");
+            System.out.println("|   2. Display events        |");
+            System.out.println("+----------------------------+");
+            System.out.println("|   3. Show occured events   |");
+            System.out.println("+----------------------------+");
+            System.out.println("|   4. Remove event          |");
+            System.out.println("+----------------------------+");
+            System.out.println("|   5. Exit                  |");
+            System.out.println("+----------------------------+");
 
+            System.out.print("\nEnter your choice number: ");
             int ch = sc.nextInt();
             switch (ch){
                 case 1:
+                    System.out.print("Event title: ");
                     String t = sc.nextLine();
+                    sc.nextLine();
+                    System.out.print("Event Description: ");
                     String d = sc.nextLine();
+                    System.out.print("Event date in yyyy-mm-dd format: ");
                     String date = sc.nextLine();
                     LocalDate date2 = LocalDate.parse(date, pattern);
                     rm.addEvent(new Event(t, d, date2));
@@ -44,8 +62,6 @@ public class Executer{
                     System.out.println("Not a valid input!");
                     break;
             }
-        }
-
-        sc.close();
+        } 
     }
 }
--- reminderManager.java
+++ reminderManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * 1. Adding events
  * 2. Viewing events
@@ -13,12 +15,12 @@ public class reminderManager{
 
     public void addEvent(Event e){
         arr.add(e);
-        System.out.println(x:"Event added to the list of events!");
+        System.out.println("Event added to the list of events!");
     }
 
     public void viewEvents(){
         if(arr.isEmpty()){
-            System.out.println(x:"Hey! there is no event to be listed.");
+            System.out.println("Hey! there is no event to be listed.");
         }
         for(int i=0; i<arr.size(); i++){
             System.out.println((i + 1) + ". " + arr.get(i).showTitle());
@@ -28,7 +30,7 @@ public class reminderManager{
 
     public void showEventAlreadyHappened(){
         if(arr.isEmpty()){
-            System.out.println(x:"No event in the list");
+            System.out.println("No event in the list");
         }
         for(int i=0; i<arr.size(); i++){
             if(arr.get(i).getEventStatus()){
@@ -40,13 +42,13 @@ public class reminderManager{
 
     public void updateEventStatus(int ind){
         if(ind <= 0 || ind >= arr.size() || arr.isEmpty()) return;
-        arr.get(ind - 1).setEventStatus(flag.true);
-        System.out.println(x: "Event marked as completed!");
+        arr.get(ind - 1).setEventStatus(true);
+        System.out.println("Event marked as completed!");
     }
 
     public void removeEvent(int ind){
         if(ind <= 0 || ind >= arr.size() || arr.isEmpty()){
-            System.out.println(x:"Not able to remove the event");
+            System.out.println("Not able to remove the event");
         }
         arr.remove(ind - 1);
     }
